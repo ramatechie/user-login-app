39c57aa095576eebcf4059fd7a68c0580d18a180
ramaki5 <rk.mundru@gmail.com>
2020-08-19 15:44:41 +0530

    init commit

---
+++ src/main/java/com/worktech/app/BootLoginApplication.java
@@ -0,0 +1,13 @@
+package com.worktech.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootLoginApplication.class, args);
+	}
+
+}
